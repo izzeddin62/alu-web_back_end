36cc73db43e159b8df1492fb89269dc7153978a9
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-09 23:08:06 +0200

    modify 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,6 +3,6 @@
 from typing import List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple(str, int)]:
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """return a list with a string and number"""
     return [(i, len(i)) for i in lst]
