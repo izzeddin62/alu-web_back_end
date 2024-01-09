be3c8431ae12162d6c5f812fcec6976eca4c7a87
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-09 22:25:44 +0200

    add docs

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""concat function"""
+
 
 def concat(str1: str, str2: str) -> str:
     """add two strings"""
--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+"""floor function"""
 import math
+
 
 def floor(n: float) -> int:
     """get the floor of a float"""
