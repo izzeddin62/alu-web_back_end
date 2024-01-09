a230a068dd1b74fbfe6d721ba72bdfbc003a7b67
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-09 22:57:50 +0200

    resolve linting error

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,9 +3,9 @@
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """make_multiplier function"""
-
 
     def multiply(n: float) -> float:
         """multiply function"""
