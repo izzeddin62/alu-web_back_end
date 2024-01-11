e30198838a4563742578894f0901bb29400e72f4
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-11 22:33:26 +0200

    modify 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Async comprehension"""
 
-import asyncio
-import random
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Async comprehension"""
     return [i async for i in async_generator()]
