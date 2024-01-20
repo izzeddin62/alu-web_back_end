d31adf59e89384de88813ff8356c86360528db0e
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-20 16:10:47 +0200

    modify 5

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,6 +3,6 @@
 
 import bcrypt
 
-def hash_password(password: str):
+def hash_password(password: str) -> bytes:
     """ encrypt password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
