1f9d1a5c113dec24c1f6fb11c36a2b7f2a752b60
izzeddin62 <i.ishimwe@alustudent.com>
2024-01-20 15:35:55 +0200

    modify 3

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ import logging
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
+
 def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
@@ -18,6 +19,7 @@ def filter_datum(fields: List[str],
                          field + "=" + redaction + separator, message)
     return message
 
+
 def get_logger() -> logging.Logger:
     """return logger"""
     logger = logging.getLogger("user_data")
@@ -27,6 +29,7 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
 
     return logger
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -46,4 +49,3 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.fields, self.REDACTION,
                             super(RedactingFormatter, self).format(record),
                             self.SEPARATOR)
-
