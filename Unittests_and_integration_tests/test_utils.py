d8807d34028d469fe76f26d8c880dbafd1e581d5
izzeddin62 <i.ishimwe@alustudent.com>
2024-02-10 15:49:50 +0200

    add 3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,9 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +28,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test for Keyerror exception """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Test for get_json """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, url, payload):
+        """ Test for get_json with mock """
+        payload = {"payload": True}
+        with patch.object(requests, 'get') as mock:
+            mock.return_value.json.return_value = payload
+            self.assertEqual(get_json(url), payload)
 
 
 if __name__ == '__main__':
