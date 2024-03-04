e44d9d4d5f497b421ab802d169facdee09b7c6fa
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-04 22:14:31 +0200

    resolve linting errors

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -5,11 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """Home page"""
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
     app.run()
-
