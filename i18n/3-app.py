43e848b7ff819b4e72e1c0f00f65d802fb012001
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-08 17:19:05 +0200

    modify 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -17,14 +17,12 @@ class Config:
 app.config.from_object(Config)
 
 
-
-
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel = Babel(app, locale_selector=get_locale)
 
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
