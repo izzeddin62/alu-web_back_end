eaa806bab2587f4a7de79469cd059be6ef7ce446
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-08 19:33:48 +0200

    add 0

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""redis instance"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """redis cache"""
+
+    def __init__(self):
+        """initialize redis instance"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    
+    def store(self, data: Union[int, float, str, bytes]) -> str:
+        """generate random key and store data in redis"""
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+  
