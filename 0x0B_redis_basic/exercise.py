243f0f3e3de36e4f7f15bbb6994c8be23fae5422
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-08 20:01:53 +0200

    add 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,19 @@
 import redis
 import uuid
 from typing import Union, Callable
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    """count calls decorator"""
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    
+    return wrapper
 
 
 class Cache:
@@ -13,12 +26,14 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate random key and store data in redis"""
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
     
+    @count_calls
     def get(self, key: str, fn: callable = None):
         """get data from redis and transform it(optional) using fn"""
         data = self._redis.get(key)
