54d3b54046bd0a62d8c76f149dce7a9fc9073fde
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-13 23:25:47 +0200

    modify 1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     id int auto_increment not null,
     email varchar(255) unique not null,
     name varchar(255),
-    country ENUM('US', 'CA', 'MX') not null default 'US',
+    country ENUM('US', 'CO', 'TN') not null default 'US',
     primary key (id)
 )
