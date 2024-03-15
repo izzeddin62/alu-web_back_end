4849da96dd46e8734a7d763c6a277bdaba03996b
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-15 11:55:49 +0200

    modify 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 SELECT
     band_name,
     CASE
-        WHEN split IS NULL THEN YEAR(curdate()) - COALESCE(formed, YEAR(curdate()))
+        WHEN split IS NULL THEN 2020 - COALESCE(formed, YEAR(curdate()))
         ELSE COALESCE(split, YEAR(curdate())) - COALESCE(formed, YEAR(curdate()))
     END AS time_together
 FROM metal_bands
