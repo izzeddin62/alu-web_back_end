83f51db4c735308e71c7afc1a31c97dbb2ea1673
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-16 23:42:28 +0200

    modify 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,21 +2,19 @@
 """ log stats """
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-logs = client.logs.nginx
-num_docs = logs.count_documents({})
-docs = list(logs.find())
-print(f"{num_docs} logs")
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    num_method = logs.count_documents({"method": method})
-    print(f"\tmethod {method}: {num_method}")
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    num_docs = logs.count_documents({})
+    docs = list(logs.find())
+    print(f"{num_docs} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {num_method}")
 
-filter_path = {"method": "GET", "path": "/status"}
-num_path = logs.count_documents(filter_path)
-print(f"{num_path} status check")
-
-# if __name__ == "__main__":
-    
+    filter_path = {"method": "GET", "path": "/status"}
+    num_path = logs.count_documents(filter_path)
+    print(f"{num_path} status check")
 
