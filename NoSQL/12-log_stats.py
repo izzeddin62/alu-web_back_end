ac9b0b9106b3ded5db8c971cf6ba67a902a415d9
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-16 23:54:50 +0200

    add 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python3
-"""Log stats - new version"""
+""" log stats """
 from pymongo import MongoClient
 
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-PIPE = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}}, {"$limit": 10}]
-
-
-def log_stats(mongo_collection, option=None):
-    """Improved 12-log_stats.py"""
-    loggers = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(loggers)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-    print("IPs:")
-
-    for ip in mongo_collection.aggregate(PIPE):
-        print(f"\t{ip.get('_id')}: {ip.get('count')}")
-
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    num_docs = logs.count_documents({})
+    docs = list(logs.find())
+    print(f"{num_docs} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {num_method}")
+
+    filter_path = {"method": "GET", "path": "/status"}
+    num_path = logs.count_documents(filter_path)
+    print(f"{num_path} status check")
+
