c3ae4c4c7f8f1743108a8703ee55d68362f7b1b4
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-16 14:31:52 +0200

    modify 6

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score
 BEGIN
     DECLARE project_id INT;
     SELECT id INTO project_id FROM projects WHERE name = project_name;
-    INSERT INTO corrections (user_id, project_name, score) VALUES (user_id, project_id, score);
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 end //
 
 
