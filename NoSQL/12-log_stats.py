d9168f7fb69a4a6eb3d511deabbd29e8ab1049b5
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-17 15:14:51 +0200

    modify 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,33 +2,31 @@
 """ log stats """
 from pymongo import MongoClient
 
-def main():
+def main(collection, options=None):
     """ log stats"""
-    try:
-        with MongoClient() as client:
-            client = MongoClient()
-            db = client.logs
-            logs = db.nginx
+    
 
-            num_logs = logs.count_documents({})
-            methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-            results = [0, 0, 0, 0, 0]
-            num_status_check = logs.count_documents({"method": "GET", "path": "/status"})
-            for method in methods:
-                num_method = logs.count_documents({"method": method})
-                results[methods.index(method)] = num_method
-            
-            print("{} logs".format(num_logs))
-            print("Methods:")
-            for method in methods:
-                print("\tmethod {}: {}".format(method, results[methods.index(method)]))
-            
-            print("{} status check".format(num_status_check))
-    except Exception as e:
-        print("there was an error")
+    num_logs = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    results = [0, 0, 0, 0, 0]
+    num_status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    for method in methods:
+        num_method = collection.count_documents({"method": method})
+        results[methods.index(method)] = num_method
+    
+    print("{} logs".format(num_logs))
+    print("Methods:")
+    for method in methods:
+        print("\tmethod {}: {}".format(method, results[methods.index(method)]))
+    
+    print("{} status check".format(num_status_check))
+
 
 
 
 
 if __name__ == "__main__":
-    main()
+    client = MongoClient()
+    db = client.logs
+    logs = db.nginx
+    main(logs)
