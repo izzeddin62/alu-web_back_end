0eac26e29cd4575220d1145ab779045b600c3ce7
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-19 21:46:40 +0200

    modify 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,9 +4,8 @@ export default async function handleProfileSignup() {
   try {
     const body = await uploadPhoto();
     const user = await createUser();
-    return `{ ${body.body} , ${user.firstName} , ${user.lastName} `;
+    console.log(`{ ${body.body} , ${user.firstName} , ${user.lastName} `);
   } catch (error) {
     console.log('Signup system offline');
-    return 'Signup system offline';
   }
 }
