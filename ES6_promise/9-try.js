8f52e9038497b216c58a93930519608fa40e382b
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-19 22:36:50 +0200

    add 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+export default function guardrail(mathFunction) {
+  const queque = [];
+  try {
+    queque.push(mathFunction());
+  } catch (error) {
+    queque.push(`Error: ${error.message}`);
+  } finally {
+    queque.push('Guardrail was processed');
+  }
+}
