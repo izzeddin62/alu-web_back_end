2cd5d7e05456521cff38672bfdc0ea67dbdca2e1
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-19 22:20:46 +0200

    add 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  const data = await Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
+  const data = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
   return data
     .map((item) => ({
       status: item.status,
