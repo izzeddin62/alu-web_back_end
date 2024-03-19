d166dd7e5ec825963325a4fa73a41383720a76c9
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-19 22:32:05 +0200

    modify 8

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    return numerator / denominator;
-  } catch (error) {
-    throw new Error('cannot divide by zero');
-  }
+  if (denominator === 0) throw new Error('cannot divide by zero');
+  return numerator / denominator;
 }
