405a062153b4d07cfb557253c18273c0239f02cc
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-28 20:58:34 +0200

    modify 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,14 @@
 /* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (!this.evacuationWarningMessage) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    if (typeof newSqft !== 'number') {
-      throw TypeError('sqft must be a number');
-    }
-    this._sqft = newSqft;
   }
 }
