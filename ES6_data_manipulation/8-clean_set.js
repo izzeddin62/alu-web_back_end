d948aabd5785b4fc9d053db97aebd63034cf985c
izzeddin62 <i.ishimwe@alustudent.com>
2024-03-29 13:01:26 +0200

    modify 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, substring) {
   if (!set || !substring) return '';
+  if (typeof set !== 'object' || typeof substring !== 'string') return '';
   return [...set].filter((el) => el.startsWith(substring)).map((el) => el.replace(substring, '')).join('-');
 }
