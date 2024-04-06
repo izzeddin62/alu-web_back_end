4f7545636d8dc18cd74678b213f69ce492496474
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-06 21:16:01 +0200

    add 10

---
+++ unittests_in_js/10-api/api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (isNaN(id)) {
+        console.log('id is not a number');
+        res.sendStatus(404);
+    } else {
+        res.send(`Payment methods for cart ${req.params.id}`);
+    }
+});
+
+app.get('/available_payments', (req, res) => {
+    res.json({
+        payment_methods: {
+            credit_cards: true,
+            paypal: false
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body, '=======')
+    // const userName = req.body.userName;
+    // console.log(userName, '=====');
+    return res.send(`Welcome `);
+});
+
+
+app.listen(7865, () => {
+    console.log('API available on localhost port 7865');
+});
