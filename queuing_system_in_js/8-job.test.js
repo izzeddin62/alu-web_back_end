9929a5c52294b760c1ea0671d863dc4f2372c9b0
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-08 21:24:55 +0200

    add 8 test

---
+++ queuing_system_in_js/8-job.test.js
@@ -0,0 +1,36 @@
+import { createQueue } from "kue"
+import createPushNotificationsJobs from "./8-job";
+import { expect } from "chai";
+
+
+describe('createPushNotificationsJobs', () => {
+    let queue
+    before(() => {
+        queue = createQueue();
+        queue.testMode.enter();
+    })
+
+    after(() => {
+        queue.testMode.clear();
+        queue.testMode.exit();
+    });
+
+    it('should create two new jobs to the queue', () => {
+        const list = [
+            {
+                phoneNumber: '4153518780',
+                message: 'This is the code 1234 to verify your account'
+            },
+            {
+                phoneNumber: '4153518781',
+                message: 'This is the code 4562 to verify your account'
+            }
+        ];
+        createPushNotificationsJobs(list, queue);
+        expect(queue.testMode.jobs.length).to.equal(2);
+    });
+
+    it('should throw an error if the jobs parameter is not an array', () => {
+        expect(() => createPushNotificationsJobs('not an array', queue)).to.throw('Jobs is not an array');
+    });
+})
