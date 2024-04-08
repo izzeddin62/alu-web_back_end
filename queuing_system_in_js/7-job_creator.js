97baebf13de7a2599b8ffeb25936bb278244c781
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-08 18:36:56 +0200

    add 7

--- queuing_system_in_js/7-job_creator.js
+++ queuing_system_in_js/7-job_creator.js
@@ -54,11 +54,12 @@ const jobs = [
     }
   })
 
-  for (let item of jobs) {
-    const job = queue.create('push_notification_code_2', item).save();
+
+jobs.forEach((item) => {
+    const job = queue.create('push_notification_code_2', item);
     job.on('enqueue', () => console.log(`Notification job created: ${job.id}`));
     job.on('complete', () => console.log(`Notification job ${job.id} completed`));
     job.on('failed', (error) => console.log(`Notification job ${job.id} failed: ${error}`));
     job.on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`));
-
-  }
+    job.save()
+});
