09b4403cded7bba318e1ac539580d5d17b56efaf
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-12 18:28:29 +0200

    modify 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data}`);
-  console.log('This important software is now closing');
+  console.log('\nThis important software is now closing');
   process.exit();
 });
