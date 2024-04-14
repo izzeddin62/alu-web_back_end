143cd47a8bf1146f0612c080da9e1ea1fe11bbcd
izzeddin62 <i.ishimwe@alustudent.com>
2024-04-14 15:15:20 +0200

    modify catch block

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -22,7 +22,7 @@ export default class StudentsController {
       });
       res.status(200).end(returnText);
     } catch (error) {
-      res.status(404).end('Cannot load the database');
+      return res.status(500).end('Cannot load the database');
     }
   }
 
@@ -32,11 +32,15 @@ export default class StudentsController {
       return res.status(500).end('Major parameter must be CS or SWE');
     }
     const path = process.argv[2];
-    const data = await readDatabase(path);
-    const lines = data.toString().split('\n').filter((line) => line.length > 0).slice(1);
-    const majorStudents = lines.filter((line) => line.split(',')[3] === major);
-    const students = majorStudents.map((student) => student.split(',')[0]);
-    const responseText = `List: ${students.join(', ')}`;
-    return res.status(200).end(responseText);
+    try {
+      const data = await readDatabase(path);
+      const lines = data.toString().split('\n').filter((line) => line.length > 0).slice(1);
+      const majorStudents = lines.filter((line) => line.split(',')[3] === major);
+      const students = majorStudents.map((student) => student.split(',')[0]);
+      const responseText = `List: ${students.join(', ')}`;
+      return res.status(200).end(responseText);
+    } catch (error) {
+      return res.status(500).end('Cannot load the database');
+    }
   }
 }
